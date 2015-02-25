925cfd0ce8da68217037949f3277f7a2c561b81a
Alexander Hopkins <achopkins@gmail.com>
2015-02-25 15:34:48 -0500

    Worked on plot_ranks

--- scripts/plot_ranks.R
+++ scripts/plot_ranks.R
@@ -1,9 +1,10 @@
 library(scales)
-#library(immunoSeqR)
-#load('ds_agg.Rda')
-n <- 500
+library(immunoSeqR)
+load('ds_agg.Rda')
+n <- 100
 #alp <- 
-tds <- ds_agg[,c(2,3,5)]
+tds <- ds_agg[,c(2,20,22)]
+name <- '7.042'
 tds <- tds[tds[,2]>0 | tds[,3]>0,]
 rownames(tds) <- seq_len(nrow(tds))
 tds$aa <- as.factor(as.character(tds$aa))
@@ -18,38 +19,21 @@ desc <- c(rep("Pre",length(pre_b)),rep("Post",length(post_b)))
 desc <- as.factor(desc)
 desc <- factor(desc, levels=rev(levels(desc)))
 dat <- c(Pre[pre_b],Post[pre_b])
-#pdf('test.pdf',width=8.5,height=8)
+pdf(paste0(name,'.pdf'),width=8.5,height=8)
 stripchart( #makes an empty chart to draw lines on
-	c(0,0)~c('Pre','Post'),
+	c(0,0)~factor(c('Pre','Post'),levels=c('Pre','Post')),
 	at=c(1.25,1.75),
 	ylim=c(3*n,1),
 	xlim=c(1.2,1.8),
 	vertical=TRUE,
 	col='white',
 	ylab='Rank',
-	frame.plot=FALSE
+	frame.plot=FALSE,
+	main=name
 	)
 for(a in 1:n){
 	segments(1.2,Pre[pre_b][a],1.7,Post[pre_b][a],col=alpha('red',0.4),lend=0)
 	segments(1.3,Pre[post_b][a],1.8,Post[post_b][a],col=alpha('blue',0.4),lend=0)
 }
-## No need to plot the points, just the segments
-#stripchart(
-#	dat ~ desc,
-#	add=TRUE,
-#	at=c(1.2,1.7),
-#	pch='.',
-#	col=alpha('red',0.5),
-#	main=paste0('Overlap: ',round(o,2)),
-#	)
-#dat <- c(Pre[post_b],Post[post_b])
-#stripchart(
-#	dat ~ desc,
-#	add=TRUE,
-#	at=c(1.3,1.8),
-#	pch='.',
-#	col=alpha('blue',0.5)
-#	)
-
-#dev.off()
+dev.off()
 
