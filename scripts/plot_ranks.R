7d6319b63d69205b74017e978bef624bc4f08106
Alexander Hopkins <achopkins@gmail.com>
2015-03-23 16:05:21 -0400

    Worked on plot_ranks

--- scripts/plot_ranks.R
+++ scripts/plot_ranks.R
@@ -1,11 +1,11 @@
 library(scales)
 library(immunoSeqR)
-load('ds_agg.Rda')
+load('~/Documents/emj/ImmunoseqResults/sampleExport.2014-07-31_10-10-24/ds_agg.Rda')
 n <- 100
-#alp <- 
-tds <- ds_agg[,c(2,20,22)]
-name <- '7.042'
+tds <- ds_agg[,c(2,4,6,3)]
+name <- '7.006'
 tds <- tds[tds[,2]>0 | tds[,3]>0,]
+tds <- tds[tds[,4]>0,] # restrict to tumor
 rownames(tds) <- seq_len(nrow(tds))
 tds$aa <- as.factor(as.character(tds$aa))
 o <- overlap(tds[,2],tds[,3]) 
@@ -14,12 +14,11 @@ Post <- rank(-tds[,3],ties.method='min')
 #plot(Pre,Post,log='xy',col=alpha('blue',0.2),pch=19,cex=0.75)
 pre_b <- which(Pre %in% 1:n)
 post_b <- which(Post %in% 1:n)
-#plot(Pre[b],Post[b])
 desc <- c(rep("Pre",length(pre_b)),rep("Post",length(post_b)))
 desc <- as.factor(desc)
 desc <- factor(desc, levels=rev(levels(desc)))
 dat <- c(Pre[pre_b],Post[pre_b])
-pdf(paste0(name,'.pdf'),width=8.5,height=8)
+pdf(paste0('~/Documents/emj/ImmunoseqResults/R/plots/Rankplots/',name,'.pdf'),width=8.5,height=8)
 stripchart( #makes an empty chart to draw lines on
 	c(0,0)~factor(c('Pre','Post'),levels=c('Pre','Post')),
 	at=c(1.25,1.75),
