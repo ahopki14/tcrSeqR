8117b84ce9da7020904290d070f45489b9680783
Alexander Hopkins <achopkins@gmail.com>
2015-03-31 17:33:20 -0400

    Removed some tracked lock files, minor changes to plotting in olScan

--- scripts/olScan.R
+++ scripts/olScan.R
@@ -12,19 +12,21 @@ for(a in seq(length(levels(dict$patient)))){
 		           which(dict$patient==name & dict$type=='Post')
 	            )
 	byN=100
+	# swap to rank by Pre or Post
   tmp <- olScan(ds[,samples[1]],ds[,samples[2]],byN=byN)
   scans[[name]] <- tmp
   #save(tmp,file=paste0('~/Documents/emj/ImmunoseqResults/olscan/',name,'_by_',byN,'.Rda'))
 }
-save(scans,file=paste0('~/Documents/emj/ImmunoseqResults/olscan/all_by_',byN,'.Rda'))
+save(scans,file=paste0('~/Documents/emj/ImmunoseqResults/olscan/all_by_',byN,'_Ranked_by_Post.Rda'))
+
 #make plotting stuff
 biggest <- max(unlist(lapply(scans,length)))
 
-pdf(file='~/Documents/emj/ImmunoseqResults/olscan/olscan.pdf',width=6,height=6)
+pdf(file='~/Documents/emj/ImmunoseqResults/olscan/olscan_Ranked_by_Post.pdf',width=6,height=6)
 plot(NA, 
   ylim=c(0,1),
-  xlim=c(1,biggest),
-  xlab='# of clones',ylab='Overlap',
+  xlim=c(1,1.02*biggest),
+  xlab='# of clones (x100)',ylab='Overlap',
   main='Overlap of top X clones')
 
 
@@ -32,6 +34,8 @@ for(a in seq(length(scans))){
 resp <- as.character(dict$response[which(dict$patient==names(scans)[a])][1])
 if(resp=='R'){color<-'blue'}else{color='red'}
 points(seq(length(scans[[a]])),scans[[a]],col=color,pch='.')
+text(length(scans[[a]]),min(scans[[a]]),paste(names(scans)[a]),pos=4,cex=0.6,offset=-0.02)
 }  
 legend('topright',legend=c('R','NR'),col=c('blue','red'),pch=19)
+
 dev.off()
