51c08d50200d711427456433da4f753a3334e2aa
Alexander Hopkins <achopkins@gmail.com>
2015-08-27 14:10:38 -0400

    Split plot_confounders away from the calculate summary stats script. Also added plot_summary_stats which is compatable with dictionaries now.

---
+++ scripts/graph_summary_stats.R
@@ -0,0 +1,53 @@
+# load dictionary and stats (see calculate_summary)
+types <- levels(dict$type)
+stats <- rbind(rep(NA,ncol(stats)),rep(NA,ncol(stats)),stats) # pad the stats with NA so dict works
+for(a in seq_along(types)){
+  w <- which(dict$type==types[a])
+  tstats <- stats[w, ]
+  tdict <- dict[w, ]
+  for(b in seq_along(tstats)){
+    # calculate p-values
+    r <- tstats[tdict$response=='R' ,b]
+    nr <- tstats[tdict$response=='NR' ,b]
+ 		p <- t.test(r,nr,var.equal=TRUE)$p.value
+ 		dir.create(paste0('/home/ahopkins/Documents/emj/ImmunoseqResults/adjuvant_study/plots/',
+ 		            colnames(tstats)[b],'/'),
+ 		            recursive=TRUE)
+ 		pdf(paste0('/home/ahopkins/Documents/emj/ImmunoseqResults/adjuvant_study/plots/',
+ 		            colnames(tstats)[b],'/',types[a],
+ 		            '.pdf'),
+ 		            width=4.5,height=8)
+ 		par(oma=c(1,2,1,1))	
+    stripchart(tstats[ ,b] ~ tdict$response,
+      at=c(1.25,1.75),
+			pch=19,
+			vertical=TRUE,
+			xlim=c(1,2),
+			ylab=colnames(tstats)[b],
+			main=paste0(types[a])
+		)
+		text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+		dev.off()
+  } 
+}
+
+## overlap
+
+resp <- dict$response[which(dict$type=='PRE')]
+r <- ol[resp=='R']
+nr <- ol[resp=='NR']
+p <- t.test(r,nr,var.equal=TRUE)$p.value
+dir.create('/home/ahopkins/Documents/emj/ImmunoseqResults/adjuvant_study/plots/Pre-Post/',
+ 		            recursive=TRUE)
+pdf('/home/ahopkins/Documents/emj/ImmunoseqResults/adjuvant_study/plots/Pre-Post/overlap.pdf',
+ 		            width=4.5,height=8)
+stripchart(ol ~ resp,
+      at=c(1.25,1.75),
+			pch=19,
+			vertical=TRUE,
+			xlim=c(1,2),
+			ylab='Pre-Post Overlap',
+		)
+text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+dev.off()
+
