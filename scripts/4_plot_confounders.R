1b4e75117dfa70fea34edd8549d8852ac64fabd5
Alexander Hopkins <achopkins@gmail.com>
2015-08-31 11:27:23 -0400

    Renamed plot_confounders, and made it more generic. Added a path variable to avoid repitition.

---
+++ scripts/4_plot_confounders.R
@@ -0,0 +1,114 @@
+# use calculate_summary script to generate stats
+# load stats and dictionary
+
+path <- '/home/ahopkins/Documents/emj/ImmunoseqResults/neoadjuvant_study/plots/'
+
+
+pdf(paste0(path,'number-vs-richness.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'cells'],stats$Richness,
+  xlab='Input Cell Number',
+  ylab='Richness')
+dev.off()
+
+pdf(paste0(path, 'number-vs-clonality.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'cells'],stats$Clonality,
+  xlab='Input Cell Number',
+  ylab='Clonality')
+dev.off()
+
+pdf(paste0(path, 'age-vs-richness.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'age'],stats$Richness,
+  xlab='Age at Diagnosis',
+  ylab='Richness')
+dev.off()
+
+pdf(paste0(path, 'age-vs-clonality.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'age'],stats$Clonality,
+  xlab='Age at Diagnosis',
+  ylab='Clonality')
+dev.off()
+
+pdf(paste0(path, 'age-vs-overlap.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'age'],stats$overlap,
+  xlab='Age at Diagnosis',
+  ylab='Overlap')
+dev.off()
+
+pdf(paste0(path , 'count-vs-overlap.pdf'),height=6,width=6)
+plot(dict[-c(1,2),'cells'],stats$overlap,
+  xlab='Input Cell Number',
+  ylab='Overlap')
+dev.off()
+
+pdf(paste0(path, 'clonality-vs-overlap.pdf'),height=6,width=6)
+plot(stats$Clonality,stats$overlap,
+  xlab='Clonality',
+  ylab='Overlap')
+dev.off()
+
+pdf(paste0(path, 'richness-vs-overlap.pdf'),height=6,width=6)
+plot(stats$Richness,stats$overlap,
+  xlab='Richness',
+  ylab='Overlap')
+dev.off()
+
+##################################Confounders######################################
+load('~/Documents/emj/ImmunoseqResults/adjuvant_study/dict.Rda')
+t <- t.test(dict$age[dict$response=='NR' & dict$type=='PRE'],dict$age[dict$response=='R' & dict$type=='PRE'])
+
+pdf(paste0(path, 'age-by-response.pdf'),height=8,width=4.5)
+stripchart(dict$age[-c(1,2)] ~ factor(dict$response[-c(1,2)]),
+	pch=19,
+	main=paste('p=',round(t$p.value,4)),
+	vertical=TRUE,
+	at=c(1.25,1.75),
+	xlim=c(1,2),
+	ylab='Age',
+	par(mai=c(1,1,1,1))
+	)
+dev.off()
+
+t <- t.test(dict$cells[dict$response=='NR' & (dict$type=='PRE')],dict$cells[dict$response=='R' & (dict$type=='PRE')])
+pdf(paste0(path, 'cells-by-response.pdf'),height=8,width=4.5)
+stripchart(dict$cells[-c(1,2)] ~ factor(dict$response[-c(1,2)]),
+	pch=19,
+	main=paste('p=',round(t$p.value,4)),
+	vertical=TRUE,
+	at=c(1.25,1.75),
+	xlim=c(1,2),
+	ylab='Input Cell Number',
+	par(mai=c(1,1,1,1))
+	)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
