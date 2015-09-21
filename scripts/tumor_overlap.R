d425d821a3d2c09822e2c758b182cece966882c1
Alexander Hopkins <achopkins@gmail.com>
2015-09-21 13:37:02 -0400

    Added script to find shared clones between samples

---
+++ scripts/tumor_overlap.R
@@ -0,0 +1,10 @@
+out <- '~/Dektop/out.txt'
+for(a in levels(as.factor(dict$patient))){
+	w <- which(dict$patient==a & (dict$type=='PRE' | dict$type=='PDAC'))
+	tds <- ds[ ,w]
+	count <- nrow(tds[tds[ ,1]>0 & tds[ ,2]>0, ])
+	total <- nrow(tds[tds[ ,2]>0 ,])
+	pct <- round(100*(count/total),2)
+	cat(paste0(as.character(a),': ', count, ' of ',total,' tumor clones...',pct,' percent\n' ),
+		   file=out,append=TRUE)
+}
