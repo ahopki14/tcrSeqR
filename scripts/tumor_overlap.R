75c430063625152a94718ff86a15539c43e79b19
Alexander Hopkins <achopkins@gmail.com>
2015-09-23 12:29:29 -0400

    Modified tumor_overlap to verfiy that samples have not been switched. It now checks the clones in each tumor agains bloods from all patients

--- scripts/tumor_overlap.R
+++ scripts/tumor_overlap.R
@@ -1,10 +1,15 @@
-out <- '~/Dektop/out.txt'
+out <- '~/Desktop/out-check.txt'
 for(a in levels(as.factor(dict$patient))){
-	w <- which(dict$patient==a & (dict$type=='PRE' | dict$type=='PDAC'))
+	for(b in levels(as.factor(dict$patient))){
+	w <- which(dict$patient==a & dict$type=='PDAC')
+	w <- c(w,which(dict$patient==b & dict$type=='PRE'))
 	tds <- ds[ ,w]
 	count <- nrow(tds[tds[ ,1]>0 & tds[ ,2]>0, ])
 	total <- nrow(tds[tds[ ,2]>0 ,])
 	pct <- round(100*(count/total),2)
-	cat(paste0(as.character(a),': ', count, ' of ',total,' tumor clones...',pct,' percent\n' ),
+	cat(paste0(as.character(a),'-',as.character(b),
+		   ': ', count, ' of ',total,' tumor clones...',pct,' percent\n' ),
 		   file=out,append=TRUE)
+	}
 }
+
