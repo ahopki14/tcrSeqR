2b9472cddfc9cf56bedba0dbe70e6e833ba7a59a
Alexander Hopkins <achopkins@gmail.com>
2015-10-03 19:46:11 -0400

    Modified graphing to use the new matricies. Now exports all overlaps and morisita overlaps

--- scripts/5_graph_summary_stats.R
+++ scripts/5_graph_summary_stats.R
@@ -2,6 +2,7 @@
 
 #path <- '/home/ahopkins/Documents/emj/ImmunoseqResults/adjuvant_study/plots/'
 
+#per sample stats
 types <- levels(dict$type)
 if(!is.na(stats[1,1])){
   stats <- rbind(rep(NA,ncol(stats)),rep(NA,ncol(stats)),stats) # pad the stats with NA so dict works
@@ -33,17 +34,42 @@ for(a in seq_along(types)){
   } 
 }
 
+
+#comparative stats
+patients <- levels(as.factor(dict$patient))
+#make empty lists
+ol_pre_post <- rep(NA,length(levels(dict$patient)))
+names(ol_pre_post) <- as.character(levels(dict$patient))
+ol_pre_tumor <- ol_pre_post
+ol_post_tumor <- ol_pre_post
+m_pre_post <- ol_pre_post
+m_pre_tumor <- ol_pre_post
+m_post_tumor <- ol_pre_post
+resp <- ol_pre_post
+
+#loop through patients and extract everything from the matricies
+for(a in seq_along(patients)){
+	pre <- which(dict$patient==patients[a] & dict$type=='PRE')-2
+	post <- which(dict$patient==patients[a] & dict$type=='POST')-2
+	tumor <- which(dict$patient==patients[a] & dict$type=='PDAC')-2
+	ol_pre_post[a] <- olm[pre,post]
+	m_pre_post[a] <- mm[pre,post]
+	ol_pre_tumor[a] <- olm[pre,tumor]
+        m_pre_tumor[a] <- mm[pre,tumor]
+	ol_post_tumor[a] <- olm[post,tumor]
+        m_post_tumor[a] <- mm[post,tumor]
+	resp[a] <- as.character(dict$response[which(dict$patient==patients[a] & dict$type=='PRE')])
+}
+resp <- as.factor(resp)
+
+
 ## overlap
-dir.create(paste0(path, 'Pre-Post/'), recursive=TRUE)
-plot(dict[-c(1,2),'age'],stats$overlap,
-  xlab='Age at Diagnosis',
-  ylab='Overlap')
-resp <- dict$response[which(dict$type=='PRE')]
-r <- ol[resp=='R']
-nr <- ol[resp=='NR']
+dir.create(paste0(path, 'Overlaps/'), recursive=TRUE)
+r <- ol_pre_post[resp=='R']
+nr <- ol_pre_post[resp=='NR']
 p <- t.test(r,nr,var.equal=TRUE)$p.value
-pdf(paste0(path,'Pre-Post/overlap.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
-stripchart(ol ~ resp,
+pdf(paste0(path,'Overlaps/overlap-pre-post.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(ol_pre_post ~ resp,
           at=c(1.25,1.75),
           pch=19,
           vertical=TRUE,
@@ -53,13 +79,40 @@ stripchart(ol ~ resp,
 text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
 dev.off()
 
-#Morisita
-resp <- dict$response[which(dict$type=='PRE')]
-r <- m[resp=='R']
-nr <- m[resp=='NR']
+r <- ol_pre_tumor[resp=='R']
+nr <- ol_pre_tumor[resp=='NR']
 p <- t.test(r,nr,var.equal=TRUE)$p.value
-pdf(paste0(path,'Pre-Post/morisita_overlap.pdf'), width=4.5,height=8,title="Pre-Post Morisita Overlap")
-stripchart(m ~ resp,
+pdf(paste0(path,'Overlaps/overlap-pre-tumor.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(ol_pre_tumor ~ resp,
+          at=c(1.25,1.75),
+          pch=19,
+          vertical=TRUE,
+          xlim=c(1,2),
+          ylab='Pre-Tumor Overlap',
+          )
+text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+dev.off()
+
+r <- ol_post_tumor[resp=='R']
+nr <- ol_post_tumor[resp=='NR']
+p <- t.test(r,nr,var.equal=TRUE)$p.value
+pdf(paste0(path,'Overlaps/overlap-post-tumor.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(ol_post_tumor ~ resp,
+          at=c(1.25,1.75),
+          pch=19,
+          vertical=TRUE,
+          xlim=c(1,2),
+          ylab='Post-Tumor Overlap',
+          )
+text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+dev.off()
+
+# Morisita
+r <- m_pre_post[resp=='R']
+nr <- m_pre_post[resp=='NR']
+p <- t.test(r,nr,var.equal=TRUE)$p.value
+pdf(paste0(path,'Overlaps/morisita-pre-post.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(m_pre_post ~ resp,
           at=c(1.25,1.75),
           pch=19,
           vertical=TRUE,
@@ -69,16 +122,31 @@ stripchart(m ~ resp,
 text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
 dev.off()
 
+r <- m_pre_tumor[resp=='R']
+nr <- m_pre_tumor[resp=='NR']
+p <- t.test(r,nr,var.equal=TRUE)$p.value
+pdf(paste0(path,'Overlaps/morisita-pre-tumor.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(m_pre_tumor ~ resp,
+          at=c(1.25,1.75),
+          pch=19,
+          vertical=TRUE,
+          xlim=c(1,2),
+          ylab='Pre-Tumor Morisita Overlap',
+          )
+text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+dev.off()
 
-
-wr <- which(dict$response=='R')
-wrn <- which(dict$response=='NR')
-rdict <- dict[wr, ]
-nrdict <- dict[wnr, ]
-rolm <- olm[wr-2,wr-2]
-rmm <- mm[wr-2,wr-2]
-nrolm <- olm[wnr-2,wnr-2]
-nrmm <- mm[wnr-2,wnr-2]
-
-
+r <- m_post_tumor[resp=='R']
+nr <- m_post_tumor[resp=='NR']
+p <- t.test(r,nr,var.equal=TRUE)$p.value
+pdf(paste0(path,'Overlaps/morisita-post-tumor.pdf'), width=4.5,height=8,title="Pre-Post Overlap")
+stripchart(m_post_tumor ~ resp,
+          at=c(1.25,1.75),
+          pch=19,
+          vertical=TRUE,
+          xlim=c(1,2),
+          ylab='Post-Tumor Morisita Overlap',
+          )
+text(1.5,max(c(r,nr)),paste0("p=",as.character(round(p,4))))
+dev.off()
 
