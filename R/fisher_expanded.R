4ebe354caa6896eb5a1c92950ba6db97348d0a79
Alexander Hopkins <achopkins@gmail.com>
2015-10-23 15:05:09 -0400

    started work on patient loops for expanded_clones, also added FDRs to the script

--- R/fisher_expanded.R
+++ R/fisher_expanded.R
@@ -1,16 +1,14 @@
 #mat <- matrix(ncol=2,sample(1:1000),100)
-out <- numeric(length=nrow(mat))
-for(a in seq(nrow(mat))){ 
-	tab <- rbind(mat[a,],apply(mat[-a,],MARGIN=2,FUN=sum))
-	out[a] <- fisher.test(tab)$p.value
-}
 
+
+w <- which(dict$patient=='7.011' & dict$type!='PDAC')
+mat <- as.matrix(ds[,w]) 
 s <- apply(mat,MARGIN=2,FUN=sum)
 exp_clone <- function(x,s){
 	tab <- rbind(x,s-x)
 	fisher.test(tab)$p.value
 }
 tm <- proc.time()
-out <- apply(mat,MARGIN=1,FUN=exp_clone,s=s)
+p_vals <- apply(mat,MARGIN=1,FUN=exp_clone,s=s)
 el<- proc.time()-tm
-
+p_adj <- p.adjust(p_vals,method='BH')
--- scripts/10_find_public_clones.R
+++ scripts/10_find_public_clones.R
@@ -1,3 +1,4 @@
 pub <- find_clones(ds=ds)
-write.table(t(dict),col.names=FALSE,file=paste0(path,'viral_clones.csv'),sep=',')
-write.table(pub,append=TRUE,file=paste0(path,'viral_clones.csv'),sep=',')
+write.table(t(dict[,c('patient','type','HLA.A')]),col.names=FALSE,
+	    file=paste0(path,'viral_clones.csv'),sep=',')
+write.table(pub,append=TRUE,file=paste0(path,'viral_clones.csv'),sep=',',col.names=FALSE)
