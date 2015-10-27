25db9b77d036230b56a2dbdc48cd13df6da63186
Alexander Hopkins <achopkins@gmail.com>
2015-10-27 14:03:57 -0400

    Work on fisher test, plots FDR cutoff etc.

--- R/fisher_expanded.R
+++ R/fisher_expanded.R
@@ -1,14 +1,46 @@
 #mat <- matrix(ncol=2,sample(1:1000),100)
 
-
-w <- which(dict$patient=='7.011' & dict$type!='PDAC')
-mat <- as.matrix(ds[,w]) 
-s <- apply(mat,MARGIN=2,FUN=sum)
-exp_clone <- function(x,s){
-	tab <- rbind(x,s-x)
-	fisher.test(tab)$p.value
+exp_clone <- function(x,s){ # x is the row of mat, s is the column sum of mat
+                           tab <- rbind(x,s-x)
+                           fisher.test(tab)$p.value
+                          }
+patients <- levels(dict$patient)
+out <- data.frame(p_adj=numeric(),ind=numeric(),patient=character())
+for(a in seq_along(patients)){
+	w <- which(dict$patient==patients[a] & dict$type!='PDAC')
+	mat <- ds[,w] 
+	mat <- mat[mat[,1]>0 | mat[,2]>0,]
+	mat <- as.matrix(mat)
+	s <- apply(mat,MARGIN=2,FUN=sum)
+	tm <- proc.time()
+	p_vals <- apply(mat,MARGIN=1,FUN=exp_clone,s=s)
+	el<- proc.time()-tm
+	p_adj <- p.adjust(p_vals,method='BH')
+	exp_cl <- length(p_adj[p_adj<0.01])
+	exp_cl_pct <- round(100*exp_cl/length(p_adj),2)
+	pdf(file=paste0(path,'Expanded_Clones/',patients[a],'-hist.pdf'),
+		width=8,height=8) 
+	hist(p_adj[p_adj<0.05],breaks=500,
+		main=paste0(patients[a],'\n',exp_cl,' clones (',exp_cl_pct,'%)'),
+		xlab='P values')
+	dev.off()
+	p_adj_ord <- p_adj[order(p_adj)]
+	tout <- data.frame(p_adj=p_adj_ord, ind=seq(length(p_adj)), 
+			   patient=rep(patients[a],length(p_adj))) 
+	out <- rbind(out,tout)
 }
-tm <- proc.time()
-p_vals <- apply(mat,MARGIN=1,FUN=exp_clone,s=s)
-el<- proc.time()-tm
-p_adj <- p.adjust(p_vals,method='BH')
+
+
+x <- seq(0,0.05,length=1000)
+
+tmp <- function(x,y){
+	length(y[y<x])
+}
+pdf('~/Desktop/plot.pdf',width=8,height=8)
+	r <-  dict$response[dict$patient==patients[a] & dict$type=='PRE'][3]
+	col <- 'red'
+	if(r=='R'){col='black'}
+	num <- sapply(x,FUN=tmp,y=p_adj)
+	plot(x,num,pch=20,col=col,main=patients[a],xlab='FDR Cutoff',
+		ylab='Number of Clones',add=TRUE)
+dev.off()
