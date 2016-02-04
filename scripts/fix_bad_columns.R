b76d593521c681eb6ec78a4d07f3d56fb4ad40aa
Alex Hopkins <achopkins@gmail.com>
2016-02-04 14:31:28 -0500

    Added fix for inconsistant labeling of counts field in tsvs

--- scripts/fix_bad_columns.R
+++ scripts/fix_bad_columns.R
@@ -3,5 +3,8 @@ for(a in all_files){
 	ds <- read.csv(a,sep='\t')
 	cols <- grep('nucleotide|aminoAcid|count|estimatedNumberGenomes',names(ds))
 	ds <- ds[,cols]
+	count <- grep('count',names(ds))
+	if(length(count)!=1){stop('Loaded incorrect number of columns for count field')}
+	if(names(ds)[count]!='count'){names(ds)[count] <- 'count'}
 	write.table(ds,file=paste0('mod/',a),sep='\t',quote=FALSE,row.names=FALSE)
 }
