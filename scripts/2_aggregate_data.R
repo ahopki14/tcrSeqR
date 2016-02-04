28e3ba215e88dc0dfe4a6e3c2540c4a7149b62d0
Alex Hopkins <achopkins@gmail.com>
2016-02-04 11:27:24 -0500

    Added removal of non productive sequences to aggregate script

--- scripts/2_aggregate_data.R
+++ scripts/2_aggregate_data.R
@@ -2,4 +2,16 @@
 setwd('/home/student/ahopkins/emj/immunoseq_data/adjuvant')
 load('ds.Rda')
 ds_agg <- iseqr_aggregate(ds,inc_nt=FALSE)
+ds <- ds_agg
+
+# Remove Empty Sequences
+length(which(ds$aa==''))
+ds <- ds[ds$aa!='',]
+
+# Remove Sequences with stop codons
+length(grep('\\*',ds$aa))
+ds <- ds[grep('\\*',ds$aa,invert=TRUE),]
+
+
+
 save(ds,file='ds_agg.Rda')
