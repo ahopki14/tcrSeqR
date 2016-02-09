f9f98f18472f1a22a37d5051e3a35f59b9f96217
Alex Hopkins <achopkins@gmail.com>
2016-02-09 11:01:50 -0500

    usage changes

--- scripts/5a_graph_summary_stats_ggplot.R
+++ scripts/5a_graph_summary_stats_ggplot.R
@@ -1,22 +1,22 @@
 plot_ds <- merge(dict,stats)
+if(nrow(plot_ds)!=nrow(ds)){stop('Could not merge, check the column names')}
+
 dir.create(paste0(path,'ggplot'))
 # set the range of variable that go on the x axis
-x_vals <- c('response','cells','age','type')
+x_vals <- c('response','arm')
 metrics <- names(stats)[names(stats)!='fn']
 for(metric in metrics){
     for(x_val in x_vals){
-        for(type in levels(dict$type)){
-            g <- ggplot(plot_ds[plot_ds$type==type,],
-            aes_q(x=as.name(x_val),y=as.name(metric))) +
-                geom_point() +
-#                xlab('') +
-                ggtitle(as.character(type)) +
-             theme_bw() +                
+        for(type in levels(plot_ds$type)){
+		g <- ggplot(plot_ds[plot_ds$type==type,],aes_q(x=as.name(x_val),y=as.name(metric))) +
+            geom_point() +
+            ggtitle(as.character(type)) +
+            theme_bw() +                
 			geom_text(aes(label=patient),colour='grey',
                         hjust=-0.5,size=2)
             plot_width= length(unique(plot_ds[,x_val]))            
 ggsave(g,file=paste0(path,'ggplot/',metric,'-by_',x_val,'-',type,'.pdf'),
-width=plot_width+0.5,height=6,units='in')
+		width=plot_width+0.5,height=6,units='in')
         }
     }
 }
