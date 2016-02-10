ff3be2178560145463720b228df89387fdf4c36b
Alex Hopkins <achopkins@gmail.com>
2016-02-09 23:19:54 -0500

    Got RMD to render with grid plots... at last.

--- scripts/plot_metrics.R
+++ scripts/plot_metrics.R
@@ -1,20 +1,24 @@
-iseqr_plot_metrics <- function(plot_ds,x_vals){	
-	metrics <- names(stats)[names(stats)!='fn']
-    #plots <- vector('list')
-    for(metric in metrics){
-        for(a in seq_along(x_vals)){
-            for(type in levels(plot_ds$type)){
-            g <- ggplot(plot_ds[plot_ds$type==type,],
-                    aes_q(x=as.name(x_vals[a]),y=as.name(metric))) +
-                geom_point() +
-                ggtitle(as.character(type)) +
-                theme_bw() +
-                geom_text(aes(label=patient),colour='grey',
-                            hjust=-0.5,size=2)
-	        #plots[[metric]] <- g
-			print(g)
-			}
-	    }
-	}
+iseqr_plot_metrics <- function(plot_ds,metric,x_val,type){	
+	g <- ggplot(plot_ds[plot_ds$type==type,],
+		aes_q(x=as.name(x_val),y=as.name(metric))) +
+    geom_point() +
+	xlab('') +
+	ggtitle(as.character(type)) +
+    theme_bw() +
+    geom_text(aes(label=patient),colour='grey',hjust=-0.5,size=2)
+	g
 }
 
+
+k <- list(
+		iseqr_plot_metrics(plot_ds,metric,x_val,'PRE'),
+		iseqr_plot_metrics(plot_ds,metric,x_val,'POST'),
+		iseqr_plot_metrics(plot_ds,metric,x_val,'PDAC')
+		)
+
+
+l <- vector('list')
+for(a in levels(plot_ds$type)){
+	l[[a]] <- iseqr_plot_metrics(plot_ds,metric,x_val,a)
+}
+do.call(grid.arrange,c(l,ncol=length(levels(plot_ds$type))))
