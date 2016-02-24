25e08f8201511e6ff4cf69164c8ab45965671ee9
Alex Hopkins <achopkins@gmail.com>
2016-02-24 17:00:04 -0500

    Added R^2 to plot_metrics, and changed plot_factor to plot points over the errorbar line

--- R/functions.R
+++ R/functions.R
@@ -323,14 +323,22 @@ out
 
 
 iseqr_plot_metrics <- function(plot_ds,metric,x_val,type,sm=TRUE){
-    g <- ggplot(plot_ds[plot_ds$type==type,],
+    # get the r^2
+	l <- lm(plot_ds[which(plot_ds$type==type),metric] ~
+            plot_ds[which(plot_ds$type==type),x_val])
+    l <- summary(l)
+    l <- l$r.squared
+	# make the plot
+	g <- ggplot(plot_ds[plot_ds$type==type,],
         aes_q(x=as.name(x_val),y=as.name(metric))) +
     geom_point() +
-    xlab('') +
+    xlab(bquote(R^2 == .(round(l,3)))) +
     ggtitle(as.character(type)) +
     theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) + 
     geom_text(aes(label=patient),colour='grey',hjust=-0.5,size=1)
-	if(sm){g <- g + geom_smooth(method=lm,alpha=0.1)}
+	if(sm){
+		g <- g + geom_smooth(method=lm,alpha=0.1)
+		}
 	g
 }
 
@@ -340,12 +348,12 @@ iseqr_plot_factor <- function(plot_ds,metric,x_val,type){
 		title <- type
 	}else{title <- ''}
     g <- ggplot(plot_ds,aes_q(x=as.name(x_val),y=as.name(metric))) +
-    geom_point() +
     xlab('') +
     ggtitle(as.character(title)) +
     theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) +
     geom_text(aes(label=patient),colour='grey',hjust=-0.5,size=1) +
-    stat_summary(fun.data=mean_95,geom='errorbar',width=0.05,colour='red',alpha=0.4)
+    stat_summary(fun.data=mean_95,geom='errorbar',width=0.05,colour='red',alpha=0.4)+
+    geom_point()
 	g
 }
 
