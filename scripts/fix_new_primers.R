370dc7e7257821f7923d3f04e36e8e8a4e4ef451
Alex Hopkins <achopkins@gmail.com>
2016-04-04 11:45:46 -0400

    Changed to fix_new_primers to deal with multiple matches. Final changes are pending call with Adaptive

--- scripts/fix_new_primers.R
+++ scripts/fix_new_primers.R
@@ -1,22 +1,21 @@
 require(immunoSeqR)
 
-dict <- readRDS('~/Documents/emj/ImmunoseqResults/mega/dict_Neoadjuvant.Rds')
-ds_nt <- readRDS('~/Documents/emj/ImmunoseqResults/mega/ds_Neoadjuvant.Rds')
+ds <- readRDS('~/Documents/emj/ImmunoseqResults/mega/ds_Adjuvant.Rds')
 
-ds <- ds_nt
-rm(ds_nt)
 ds$aa <- as.character(ds$aa)
 w <- grep("\\*",ds$aa)
 if(length(w)>0){ds <- ds[-w,]} #remove clones with stop codons
+paste('Removed ',length(w),' clones with stop codons')
 w <- which(ds$aa =='')
 if(length(w)>0){ds <- ds[-w,]} #remove clones with no translation
-
+paste('Removed ',length(w),' clones with no translation')
+paste0('Leaving ',nrow(ds),' clones \n')
 
 ds$nt <- as.character(ds$nt)
 len <- sapply(ds$nt,nchar)
 w <- which(len<87)
 md <- grep('aa|nt',names(ds))
-colSums(ds[w,-md]) # should be zero for all type!=PDAC
+#colSums(ds[w,-md]) # should be zero for all type!=PDAC
 
 ind <- split(seq_len(nrow(ds)),ds$aa)
 syn <- sapply(ind,length)
@@ -24,30 +23,46 @@ syn <- sapply(ind,length)
 w_syn <- which(syn>1)
 to_fix <- ind[w_syn]
 
+gc()
 
 to_toss <- vector()
+idk <- list()
 for(a in seq_along(to_fix)){
-	tds <- ds[to_fix[[a]],]
-	tlen <- as.vector(sapply(tds$nt,nchar))
-	if(nrow(tds)==1){stop('Only one value found')}
-	if(nrow(tds)==2){
+#	tds <- ds[to_fix[[a]],]
+	n <- length(to_fix[[a]])
+	tlen <- as.vector(sapply(ds$nt[to_fix[[a]]],nchar))
+	if(n==1){stop('Only one value found')}
+	if(n==2){
 		if(tlen[1]!=tlen[2]){
 			keep <- which.max(tlen)
 			toss <- which.min(tlen)
 			k.ind <- to_fix[[a]][keep]
 			t.ind <- to_fix[[a]][toss]
-			ds[k.ind,-md] <- colSums(tds[,-md])
-			to_toss <- c(to_toss,t.ind)
+			if(grepl(ds$nt[t.ind],ds$nt[k.ind])){
+				ds[k.ind,-md] <- colSums(ds[c(k.ind,t.ind),-md])
+				to_toss <- c(to_toss,t.ind)
+			}
 		}
 	}
-	if(nrow(tds)>2){
-		w <- which(tlen<max(tlen))
-		for(b in w){
-			#find match and collapse
-			
+	if(n>2){
+		w_toss <- which(tlen<max(tlen))
+		for(toss in w_toss){
+			keep <- grep(ds$nt[to_fix[[a]][toss]],ds$nt[to_fix[[a]][-toss]])
+			if(length(keep)>1){idk[[a]] <- to_fix[[a]]}
+			if(length(keep)==1){
+				k.ind <- to_fix[[a]][keep]	
+				t.ind <- to_fix[[a]][toss]
+				ds[k.ind,-md] <- colSums(ds[c(k.ind,t.ind),-md])
+				to_toss <- c(to_toss,t.ind)
+			}
 		}	
 	}
 }
 ds <- ds[-to_toss,]
+paste('Ignored ',length(idk),' Due to multiple matches')
+head(idk)
+paste('Collapsed',length(to_toss),'clones')
+
+saveRDS(ds,'~/Desktop/ds_Adjuvant_fixed.Rds')
 
 
