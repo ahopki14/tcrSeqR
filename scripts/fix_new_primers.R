884083b4dbfaae28ff6a60a7424ba766e67185a2
Alex Hopkins <achopkins@gmail.com>
2016-04-08 12:18:16 -0400

    Re-wrote fix_new primers to address a few bugs and make it faster.

--- scripts/fix_new_primers.R
+++ scripts/fix_new_primers.R
@@ -1,6 +1,6 @@
 require(immunoSeqR)
 
-ds <- readRDS('~/Documents/emj/ImmunoseqResults/mega/ds_Adjuvant.Rds')
+ds <- readRDS('~/Documents/emj/ImmunoseqResults/mega/ds_Neoadjuvant.Rds')
 
 ds$aa <- as.character(ds$aa)
 w <- grep("\\*",ds$aa)
@@ -23,58 +23,51 @@ syn <- sapply(ind,length)
 w_syn <- which(syn>1)
 to_fix <- ind[w_syn]
 
+get_len <- function(x){as.vector(sapply(ds$nt[x],nchar))}
+lens <- sapply(to_fix,get_len)
+len_equiv <- function(x){min(lens[[x]])==max(lens[[x]])}
+w <- sapply(seq(length(lens)),len_equiv)
+
+to_fix <- to_fix[!w] # only groups where different lengths are present
+lens <- lens[!w]
+print(paste('Found ',length(lens), 'clones to collapse'))
+
 gc()
 
 to_toss <- vector()
 idk <- list()
+idk.c <- 1
+nomatch <- list()
 for(a in seq_along(to_fix)){
 #	tds <- ds[to_fix[[a]],]
-	n <- length(to_fix[[a]])
-	tlen <- as.vector(sapply(ds$nt[to_fix[[a]]],nchar))
-	if(n==1){stop('Only one value found')}
-	if(n==2){
-		if(tlen[1]!=tlen[2]){
-			keep <- which.max(tlen)
-			toss <- which.min(tlen)
-			k.ind <- to_fix[[a]][keep]
-			t.ind <- to_fix[[a]][toss]
-			if(grepl(ds$nt[t.ind],ds$nt[k.ind])){
+#	n <- length(to_fix[[a]])
+	tlen <- lens[[a]]
+	w.toss <- which(tlen<max(tlen))
+	for(b in w.toss){
+		w.match <- grep(ds$nt[to_fix[[a]][b]],ds$nt[to_fix[[a]]])
+		w.match <- w.match[which(w.match!=b)] #so it doesn't match itself
+		if(length(w.match)==0){
+			nomatch <- c(nomatch,to_fix[[a]][b])
+		}else{
+			counts <- as.numeric(rowSums(ds[to_fix[[a]][w.match],-md]))
+			w.big <- which(counts==max(counts))
+			if(length(w.big)==1){
+				k.ind <- to_fix[[a]][w.match[w.big]]
+				t.ind <- to_fix[[a]][b]
 				ds[k.ind,-md] <- colSums(ds[c(k.ind,t.ind),-md])
 				to_toss <- c(to_toss,t.ind)
-			}
+			}else{
+					idk[idk.c] <- to_fix[[a]][b]
+					idk.c <- idk.c + 1
+				}
 		}
-	}
-	if(n>2){
-		w_toss <- which(tlen<max(tlen))
-		for(toss in w_toss){
-			keep <- grep(ds$nt[to_fix[[a]][toss]],ds$nt[to_fix[[a]][-toss]])
-			if(length(keep)==1){
-				k.ind <- to_fix[[a]][keep]	
-				t.ind <- to_fix[[a]][toss]
-				ds[k.ind,-md] <- colSums(ds[c(k.ind,t.ind),-md])
-				to_toss <- c(to_toss,t.ind)
-			}
-			if(length(keep)>1){
-				s <- rowSums(ds[to_fix[[a]][keep],-md])
-				biggest <- which(s==max(s))
-				if(length(biggest==1)){
-					k.ind <- to_fix[[a]][keep][biggest]
-					t.ind <- to_fix[[a]][toss]
-                	ds[k.ind,-md] <- colSums(ds[c(k.ind,t.ind),-md])
-                	to_toss <- c(to_toss,t.ind)	
-				}
-				if(length(biggest>1)){
-					idk[[a]] <- to_fix[[a]]
-				}
-			}
-		}	
-	}
+	}	
 }
 ds <- ds[-to_toss,]
 paste('Ignored ',length(idk),' Due to multiple matches')
-head(idk)
-paste('Collapsed',length(to_toss),'clones')
+paste('Collapsed',length(to_toss),'clones (',round(100*length(to_toss)/length(to_fix),2),'%)')
+paste('Failed to collapse', length(nomatch), 'clones')
 
-saveRDS(ds,'~/Desktop/ds_Adjuvant_fixed.Rds')
+saveRDS(ds,'~/Documents/emj/ImmunoseqResults/mega/fixed/ds_Neodjuvant_fixed.Rds')
 
 
