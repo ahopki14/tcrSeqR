b1a5e24c385bd087d1cd5d2eb6619565fcb08e3f
Alex Hopkins <achopkins@gmail.com>
2016-06-13 23:15:01 -0400

    Added a lot to script 3. Now adds expanded clones, and change in metrics

--- scripts/core/3_calculate_summary_stats.R
+++ scripts/core/3_calculate_summary_stats.R
@@ -1,5 +1,5 @@
 # change to dir
-setwd('~/Documents/emj/ImmunoseqResults/data/neoadjuvant/')
+setwd('~/Documents/emj/ImmunoseqResults/data/baseline/')
 
 # load data and dictionary
 ds <- readRDS('ds_agg.Rds')
@@ -24,7 +24,33 @@ saveRDS(stats,file='stats.Rds')
 plot_ds <- merge(dict,stats)
 
 #calculate change in metrics
-plot_ds <- delta_stats(plot_ds,'PRE','POST','type',names(stats)[1:3],'patient',merge=TRUE)
+
+#for adjuvant and neoadjuvant
+#plot_ds <- delta_stats(plot_ds,'PRE','POST','type',names(stats)[1:3],'patient',merge=TRUE)
+
+#for sbrt
+tmp2 <- delta_stats(plot_ds,'PRETREAT','POSTFOLF','type',names(stats)[1:3],'patient',merge=TRUE)
+plot_ds <- delta_stats(plot_ds,'PRETREAT','POSTSBRT','type',names(stats)[1:3],'patient',merge=TRUE)
+
+w <- which(plot_ds$type=='POSTFOLF')
+m_ind_1 <- grep('Log2',names(plot_ds))
+m_ind_2 <- grep('Log2',names(tmp2))
+
+plot_ds[w,m_ind_1] <- tmp2[w,m_ind_2]
+
+#for ipi
+plot_ds <- merge(dict,stats)
+plot_ds$patient <- as.factor(plot_ds$patient)
+tmp2 <- delta_stats(plot_ds,'PRE','POST3','type',names(stats)[1:3],'patient',merge=TRUE)
+plot_ds <- delta_stats(plot_ds,'PRE','POST1','type',names(stats)[1:3],'patient',merge=TRUE)
+
+w <- which(plot_ds$type=='POST3')
+m_ind_1 <- grep('Log2',names(plot_ds))
+m_ind_2 <- grep('Log2',names(tmp2))
+
+plot_ds[w,m_ind_1] <- tmp2[w,m_ind_2]
+
+
 
 
 # Calculate the overlap and morisitas
@@ -50,8 +76,54 @@ mm <- as.matrix(mm)
 save(olm,mm,file=paste0(path,'olm.Rda'))
 
 # Calculate Expanded Clones
-out <- iseqr_exp_cl(ds,dict,s1='PRE',s2='POST',category='type',by='patient',inc.all=FALSE)
-plot_ds <- merge(plot_ds,exp_clones,by=c('patient','type'),all=TRUE)
+
+
+# for adjuvant and neoadjuvant
+#plot_ds <- merge(plot_ds,exp_clones,by=c('patient','type'),all=TRUE)
+out <- iseqr_exp_cl(ds,dict,s1='PRETREAT',s2='POSTSBRT',category='type',by='patient',inc.all=FALSE)
+exp_clones <- out
+
+plot_ds[,'Number of Expanded Clones vs PRE'] <- rep(NA,nrow(plot_ds))
+col <- grep('Number of Expanded Clones',names(plot_ds))
+for(a in seq(nrow(exp_clones))){
+  w <- which(plot_ds$patient==exp_clones$patient[a] & plot_ds$type==exp_clones$type[a])
+  plot_ds[w,col] <- exp_clones[a,1]
+}
+
+# and again for POSTFOLF
+out <- iseqr_exp_cl(ds,dict,s1='PRETREAT',s2='POSTFOLF',category='type',by='patient',inc.all=FALSE)
+exp_clones <- out
+
+col <- grep('Number of Expanded Clones',names(plot_ds))
+for(a in seq(nrow(exp_clones))){
+  w <- which(plot_ds$patient==exp_clones$patient[a] & plot_ds$type==exp_clones$type[a])
+  plot_ds[w,col] <- exp_clones[a,1]
+}
+
+
+# for ipi
+out <- iseqr_exp_cl(ds,dict,s1='PRE',s2='POST1',category='type',by='patient',inc.all=FALSE)
+exp_clones <- out
+
+plot_ds[,'Number of Expanded Clones vs PRE'] <- rep(NA,nrow(plot_ds))
+col <- grep('Number of Expanded Clones',names(plot_ds))
+for(a in seq(nrow(exp_clones))){
+  w <- which(plot_ds$patient==exp_clones$patient[a] & plot_ds$type==exp_clones$type[a])
+  plot_ds[w,col] <- exp_clones[a,1]
+}
+
+# and again for POST3
+out <- iseqr_exp_cl(ds,dict,s1='PRE',s2='POST3',category='type',by='patient',inc.all=FALSE)
+exp_clones <- out
+
+col <- grep('Number of Expanded Clones',names(plot_ds))
+for(a in seq(nrow(exp_clones))){
+  w <- which(plot_ds$patient==exp_clones$patient[a] & plot_ds$type==exp_clones$type[a])
+  plot_ds[w,col] <- exp_clones[a,1]
+}
+
+
+
 saveRDS(plot_ds,'plot_ds.Rds')
 
 
