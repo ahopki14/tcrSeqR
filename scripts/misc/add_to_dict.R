a67cc7bbf8be6e368193b118067bab8b4ff78538
Alex Hopkins <achopkins@gmail.com>
2017-01-19 10:36:05 -0500

    Small runtime changes

--- scripts/misc/add_to_dict.R
+++ scripts/misc/add_to_dict.R
@@ -1,10 +1,7 @@
-dict$age <- rep(NA,nrow(dict))
-dict$os <- rep(NA,nrow(dict))
-dict$ptv80 <- rep(NA,nrow(dict))
+plot_ds$arm <- rep(NA,nrow(plot_ds))
 
 
-for(a in levels(dict$patient)){
-	dict$age[which(dict$patient==a)] <- tmp$age_at_diagnosis[which(tmp$patient==a)] 
-	dict$os[which(dict$patient==a)] <- tmp$os[which(tmp$patient==a)] 
-	dict$ptv80[which(dict$patient==a)] <- tmp$PTV80[which(tmp$patient==a)] 
+for(a in levels(plot_ds$patient)){
+	plot_ds$arm[which(plot_ds$patient==a)] <- rep(arm$arm[which(arm$patient==a)],
+							length(plot_ds$arm[which(plot_ds$patient==a)]))
 }
