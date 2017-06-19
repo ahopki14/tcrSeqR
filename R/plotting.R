5db7e54cd8d6690b3f08129fd22669f7cd43caa6
Alex Hopkins <achopkins@gmail.com>
2017-06-19 14:09:55 -0400

    changed plot functions to accept metric names with dots instead of spaces

--- R/plotting.R
+++ R/plotting.R
@@ -1,15 +1,16 @@
-iseqr_plot_metrics <- function(plot_ds,metric,x_val,type,sm=TRUE,hjust=-0.5,labels=TRUE){
+iseqr_plot_metrics <- function(plot_ds,metric,x_val,type=NA,sm=TRUE,hjust=-0.5,labels=TRUE){
+  if(!is.na(type)){
+    plot_ds <- plot_ds[plot_ds$type==type,]
+    title <- type
+  }else{title <- ''}
     # get the r^2
-  l <- lm(plot_ds[which(plot_ds$type==type),metric] ~
-            plot_ds[which(plot_ds$type==type),x_val])
-    l <- summary(l)
-    l <- l$r.squared
+  l <- lm(plot_ds[,metric] ~ plot_ds[,x_val])
+    l <- summary(l)$r.squared
   # make the plot
-  g <- ggplot(plot_ds[plot_ds$type==type,],
-        aes_q(x=as.name(x_val),y=as.name(metric))) +
+  g <- ggplot(plot_ds, aes_q(x=as.name(x_val),y=as.name(metric))) +
     geom_point() +
     xlab(bquote(R^2 == .(round(l,3)))) +
-    ggtitle(as.character(type)) +
+    ggtitle(title) +
     theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
   if(labels){g <- g + geom_text(aes(label=patient),colour='grey',hjust=hjust,size=1)}
   if(sm){
@@ -24,7 +25,7 @@ iseqr_plot_factor <- function(plot_ds,metric,x_val,type=NA,hjust=-0.5,labels=TRU
     title <- type
   }else{title <- ''}
     g <- ggplot(plot_ds,aes_q(x=as.name(x_val),y=as.name(metric))) +
-    xlab('') +
+    xlab('') + ylab(gsub('\\.',' ', metric)) + 
     ggtitle(as.character(title)) +
     theme_bw() + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()) +
     stat_summary(fun.data=mean_95,geom='errorbar',width=0.05,colour='red')+
