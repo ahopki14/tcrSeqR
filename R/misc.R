a7d8f9e059167c2ed6e5b1a6c76dcb5bfd5d172a
Alex Hopkins <achopkins@gmail.com>
2017-06-19 14:08:45 -0400

    Added stars function for plotting p-vals

--- R/misc.R
+++ R/misc.R
@@ -185,3 +185,20 @@ biocload <- function(){
 source("https://bioconductor.org/biocLite.R")
 }
 
+
+stars <- function(t){
+	p <- t$p.value
+	stopifnot(class(p) == 'numeric')
+	if(p<=0.001){
+		'***'
+	}else if(p<=0.01){
+		'**'
+	}else if(p<=0.05){
+		'*'
+	} else if(p>0.05){
+		''
+	}
+}
+
+}
+
