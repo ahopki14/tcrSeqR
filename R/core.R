26aceb04f3e8e97c5cd703144afc17a022e8df45
Alex Hopkins <achopkins@gmail.com>
2017-08-29 13:12:53 -0400

    Added function to make SummarizedExperiment object from ds

--- R/core.R
+++ R/core.R
@@ -110,3 +110,39 @@ iseqr_aggregate <- function(ds, inc_nt=TRUE){
     ds_out
 }
 
+iseqr_make_se <- function(ds_agg, dict){
+	require(SummarizedExperiment)
+	#reoder the dictionary to match the ds
+	dict <- iseqr_order(dict, ds_agg, reorder=T)
+	#get metadata and data column locations
+	w_m <- grep('aa|nt|syn', names(ds_agg))
+	w_d <- grep('aa|nt|syn', names(ds_agg), invert=T)
+	# make the SE object with the data
+	ds_se <- SummarizedExperiment(assays=list(tcr=as.matrix(ds_agg[,w_d], rownames=F)))
+	# add some metadata
+	colnames(se) <- names(ds[,w_d])
+	rownames(ds_se) <- ds_agg$aa
+	rowData(ds_se) <- DataFrame(ds_agg[,w_m])
+	stopifnot(all(colnames(ds_se)==dict$fn))
+	colData(ds_se) <- DataFrame(dict)
+	#return the SE object
+	ds_se
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
