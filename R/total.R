ad77d91c43a6399902e8ba05b139889742354b86
Alex Hopkins <achopkins@gmail.com>
2017-10-04 22:12:26 -0400

    made Total.Sequences function

---
+++ R/total.R
@@ -0,0 +1,31 @@
+#' Total sequences
+#' 
+#' Computes the sum of a vector
+#' @param x A numeric vector or tcr object
+#' @param merge If x is a tcr object, the output will be a tcr object with the
+#' richness merged into the metadata. 
+#' @return If x is a vector, the function returns the sum. If x is a tcr
+#' object, and merge=T, then the function returns a tcr object with
+#' Total.Sequences included in the metadata for all samples.  
+#' @author Alexander Hopkins
+#' @export
+total <- function(x,...){
+	sum(x)
+}
+setMethod("total", "tcr",
+	  function(x, merge=T){
+		tot <- apply(assay(x),FUN=total, MARGIN=2)
+	  	df <- data.frame(Total.Sequences=tot,fn=names(tot))
+		if(merge){
+		tmp <- DataFrame(
+					merge(colData(x),df,by='fn', all.x=T)
+					)
+		# This is necessary to maintain colnames(assay(ds))
+		# for some reason...
+		rownames(tmp) <- tmp$fn
+		colData(x) <- tmp
+		return(x)
+		}
+		else{tot}
+	  }
+  )
