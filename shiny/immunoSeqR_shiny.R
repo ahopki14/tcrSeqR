86fe37116c9cf1f23957e8329bc2340bba4bd9f2
Alex Hopkins <achopkins@gmail.com>
2017-10-09 15:25:08 -0400

    updated shiny script to accept tcr objects

--- shiny/immunoSeqR_shiny.R
+++ shiny/immunoSeqR_shiny.R
@@ -1,15 +1,16 @@
 library(shiny)
-library(ggplot2)
-library(immunoSeqR)
-plot_ds <- readRDS('~/Documents/emj/ImmunoseqResults/data/ipi/plot_ds.Rds')
 
-timepoints <- levels(plot_ds$type)
-metrics <- names(plot_ds)[11:16]
-x_vals <- names(plot_ds)[6:8]
+#load a tcr object called 'ds'
+
+timepoints <- levels(ds$type)
+metrics <- c("Clonality", "Number.of.Expanded.Clones",
+	     "Richness","Total.Sequences","Morisita",
+	     "Log2.Fold.Change.in.Clonality","Log2.Fold.Change.in.Richness")
+x_vals <- c('response', 'age','os.m','arm','nonskin.irae','sex')
 
 
 ui <- fluidPage(
-		titlePanel('immunoSeqR'),
+		titlePanel('immunoSeqR Shiny'),
 		sidebarLayout(
 		  sidebarPanel(
 		  	selectInput(inputId='metric', label='Metric', choices=metrics),
@@ -25,13 +26,24 @@ ui <- fluidPage(
 
 server <- function(input, output){
 	output$p <- renderPlot({
-		g <- iseqr_plot_factor(plot_ds, 
-				       metric=input$metric, 
-				       x_val=input$x_val, 
-				       type=input$type, 
-				       labels=F)
-		g + xlab(input$x_val)
-		#hist(rnorm(input$t))
+		if(class(colData(ds)[,input$x_val])=='factor' | 
+		   class(colData(ds)[,input$x_val])=='character' ){
+			g <- iseqr_plot_factor(ds, 
+					       metric=input$metric, 
+					       by=input$x_val, 
+					       type=input$type, 
+					       labels=F)
+			g + xlab(input$x_val)
+		}else if(class(colData(ds)[,input$x_val])=='integer' | 
+			 class(colData(ds)[,input$x_val])=='numeric'){
+			g <- iseqr_plot_metrics(ds, 
+					       metric=input$metric, 
+					       by=input$x_val, 
+					       type=input$type, 
+					       labels=F)
+			g + xlab(input$x_val)
+
+		}
 	})
 
 }
