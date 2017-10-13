873d6185ad87f95d1031190f19832413919af9cb
Alex Hopkins <achopkins@gmail.com>
2017-10-13 09:53:10 -0400

    filter methods should work now

--- R/filter.DataFrame.R
+++ R/filter.DataFrame.R
@@ -4,6 +4,10 @@
 #' @author Alexander Hopkins
 #' @export
 #' @method filter DataFrame
+filter = function(x,...)
+{
+	    UseMethod("filter")
+}
 filter.DataFrame <- function(.data,...) {
 	    DataFrame(filter(as.data.frame(.data),...))
 }
