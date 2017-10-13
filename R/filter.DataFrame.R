b1a8aa5a7bef1cafa7f54eb37ed483af7386eb1b
Alex Hopkins <achopkins@gmail.com>
2017-10-13 09:47:11 -0400

    Still trying to get filter recognized as s3 methods

--- R/filter.DataFrame.R
+++ R/filter.DataFrame.R
@@ -3,7 +3,7 @@
 #' Method for using dplyr filter on DataFrames
 #' @author Alexander Hopkins
 #' @export
-#' @method dplyr::filter DataFrame
+#' @method filter DataFrame
 filter.DataFrame <- function(.data,...) {
 	    DataFrame(filter(as.data.frame(.data),...))
 }
