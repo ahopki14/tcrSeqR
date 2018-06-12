9ebc2e2676d691e0e3a108d23fdc96e918fe1ca2
Alex Hopkins <achopkins@gmail.com>
2018-06-12 13:00:31 -0400

    Added example_ds in rda format and added R file to document

---
+++ R/example_ds.R
@@ -0,0 +1,7 @@
+#' Example tcr Class Data
+#' @docType data
+#' @format tcr
+#' @source A downsampled selection of data from a TCR sequencing experiment
+#' @examples
+#' data(example_ds)
+"example_ds"
